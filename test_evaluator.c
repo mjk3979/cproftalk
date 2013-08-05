4d66fef6604d12822873eaf37df5119f57c9c027
Matthew Koontz <mjk3979@rit.edu>
2013-08-04 23:56:14 -0400

    Started work on a printer for proftalk_expr_ts.

--- test_evaluator.c
+++ test_evaluator.c
@@ -4,14 +4,29 @@
 #include "lexer.h"
 #include "evaluator.h"
 
+static void print_expr(proftalk_expr_t expr)
+{
+	if (expr.value == LIST_TYPE)
+	{
+		printf("( ");
+
+		while(expr.expressions.head != NULL)
+			print_expr(*(proftalk_expr_t *)pop(&expr.expressions));
+		printf(") ");
+	}
+	else
+	{
+		printf("%d ", expr.value);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	int tokenCount;
 	ll_t tokens = parse(stdin, &tokenCount);
 	proftalk_expr_t expr = lex(&tokens);
 	global_env = makeEnv(NULL);
-	int result = eval(expr, &global_env).value;
-	printf("%d\n", result);
+	print_expr(eval(expr, &global_env));
 	
 	return 0;
 }
